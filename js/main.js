13b7901d412b38795250c54cc21a39e23b1cd4d6
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-04-28 16:44:39 -0400

    Rectangles removed added only to the h2s.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,50 @@ $('.pathorange').on('click', function (e) {
   $('.pathblue').addClass('grows');
 });
 
-$('.ontext').on('click', function (e) {
+$('.onleft').on('click', function (e) {
   $('#leader').addClass('appear');
 });
 
+$('.onleft').on('click', function (e) {
+  $('#folder').addClass('appear');
+});
+
+$('.onleft').on('click', function (e) {
+  $('#audio').addClass('appear');
+});
+
+$('.onleft').on('click', function (e) {
+  $('#free').addClass('appear');
+});
+
+$('.onleft').on('click', function (e) {
+  $('#alone').addClass('appear');
+});
+
+$('.onleft').on('click', function (e) {
+  $('#graph').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#hourglass').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#letter').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#direction').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#chart').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#cloud').addClass('appear');
+});
+
+$('.onright').on('click', function (e) {
+  $('#microphone').addClass('appear');
+});
